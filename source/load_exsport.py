4a3a3fa45e5ef01872e7edb6e7bc87ef15475500
Zulfa Nurfajar <znurfajar@gmail.com>
2025-04-03 11:23:15 +0700

    update

--- source/load_exsport.py
+++ source/load_exsport.py
@@ -8,20 +8,20 @@ class LoadExsportDatabase(luigi.Task):
         return TransformTokpedExportData(), ExtractUserData(), ExtractOrderData(), ExtractReviewOrder() 
     
     def output(self):
-        return print("All processes completed successfully!")
+        return print('All processes completed successfully!')
     
     def run(self):
         create_database()
         create_tables()
 
-        load_data_from_csv("df_category_exsport", self.input()[0][4].path)
-        load_data_from_csv("df_color_exsport", self.input()[0][3].path)
-        load_data_from_csv("df_name_exsport", self.input()[0][2].path)
-        load_data_from_csv("df_product_exsport", self.input()[0][0].path)
-        load_data_from_csv("df_stock_exsport", self.input()[0][1].path)
-        load_data_from_csv("df_user_tokped", self.input()[1].path)
-        load_data_from_csv("df_order_tokped", self.input()[2].path)
-        load_data_from_csv("df_review_order_tokped", self.input()[3].path)
+        load_data_from_csv('df_category_exsport', self.input()[0][4].path)
+        load_data_from_csv('df_color_exsport', self.input()[0][3].path)
+        load_data_from_csv('df_name_exsport', self.input()[0][2].path)
+        load_data_from_csv('df_product_exsport', self.input()[0][0].path)
+        load_data_from_csv('df_stock_exsport', self.input()[0][1].path)
+        load_data_from_csv('df_user_tokped', self.input()[1].path)
+        load_data_from_csv('df_order_tokped', self.input()[2].path)
+        load_data_from_csv('df_review_order_tokped', self.input()[3].path)
 
 if __name__ == '__main__':
     luigi.run([LoadExsportDatabase()], workers=1, local_scheduler=True)
